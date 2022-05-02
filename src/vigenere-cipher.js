2c72fa86df6ee1688f9df3054bf046d3c08eeffc
VitalikSamusenko <vitaliksamusenko1997@gmail.com>
2022-05-02 16:19:19 +0300

    feat: complete last tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (direction) {
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+ encrypt(str, key) {
+    if (str == undefined || key == undefined) {
+      throw new Error ('Incorrect arguments!');
+    }
+      let klstr = "";
+      let res = "";
+      let amount = 0;
+      let count = 0;
+
+      for (let i = 0; i < str.length; i++) {
+        if (str.toUpperCase().charCodeAt(i) > 64 && str.toUpperCase().charCodeAt(i) < 91) {
+          if ((i - count - (key.length * amount)) === key.length) 
+          {
+            amount++;
+          }
+          klstr += key.toUpperCase()[i - count - key.length * amount];
+        } else {
+          klstr += " ";
+          count++;          
+        }
+      }
+      for (let i = 0; i < str.length; i++) {
+        if (str.toUpperCase().charCodeAt(i) > 64 && str.toUpperCase().charCodeAt(i) < 91) {
+          let index = str.toUpperCase().charCodeAt(i) + klstr.charCodeAt(i) - 65;
+          if (index > 90) {
+            index -= 26;
+          }
+          res += String.fromCharCode(index);
+        } else res += str.toUpperCase()[i];
+      }
+      if (this.direction === false) {
+        return res.split('').reverse().join('');
+      }
+      return res;
+    }
+
+ decrypt(str, key) {
+      if (str == undefined || key == undefined) {
+        throw new Error ('Incorrect arguments!');
+      }
+      let klstr = "";
+      let res = "";
+      let amount = 0;
+      let count = 0;
+
+      for (let i = 0; i < str.length; i++) {
+        if (str.toUpperCase().charCodeAt(i) >= 65 && str.toUpperCase().charCodeAt(i) <= 90) {
+          if ((i - count - key.length * amount) === key.length) {
+            amount++;
+          }
+          klstr += key.toUpperCase()[(i - count) - key.length * amount];
+        } else {
+          klstr += " ";
+          count++;
+        }
+      }
+      for (let i = 0; i < str.length; i++) {
+        if (str.toUpperCase().charCodeAt(i) >= 65 && str.toUpperCase().charCodeAt(i) <= 90) {
+          let index = str.toUpperCase().charCodeAt(i) - klstr.charCodeAt(i) + 65;
+          if (index < 65) {
+            index += 26;
+          }
+          res += String.fromCharCode(index);
+        } else res += str.toUpperCase()[i];
+      }
+      if (this.direction === false) {
+        return res.split('').reverse().join('');
+      }
+      return res;
+    }
 }
 
 module.exports = {
